df19d3b0c53a85b48ddc4515057c06a99ad9cc75
anas <***@***.com>
2023-03-18 12:24:12 -0700

    printing all combinations of two numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,12 @@
 int main ()
 {
 
-int i,j;
-for (i=0,i<9,i++)
-     for (j=9;j>i;j--)
-        putchar(i(10+i-j),\t);
-        
+	int i,j;
+	for (i=0,i<9,i++){
+		for (j=9;j>i;j--){
+	        	putchar(i(10+i-j),\t);
+                             }
+                       }
 return (0);
 
 }
