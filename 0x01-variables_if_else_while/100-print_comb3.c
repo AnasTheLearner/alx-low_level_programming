ab5b7ba20e2480ffa4ee923b02a6db0ea12648ee
anas <***@***.com>
2023-03-18 12:16:31 -0700

    printing all combinations of two numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+/*
+ * file: task 10
+ */
+
+#include <stdio.h>
+/**
+ * main : print random combination
+ */
+int main ()
+{
+
+int i,j;
+for (i=0,i<9,i++)
+     for (j=9;j>i;j--)
+        putchar(i(10+i-j),\t);
+        
+return (0);
+
+}
