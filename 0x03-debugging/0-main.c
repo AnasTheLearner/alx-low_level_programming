50d0074da398e5845772189a75d0badbdaa82573
anas <***@***.com>
2023-03-22 09:16:27 -0700

    First commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * task 0
+ */
+
+/**
+ * main - function test if it orints an integer positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+
+	return (0);
+}
