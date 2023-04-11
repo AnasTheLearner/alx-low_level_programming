af47a34e072f584002a3f02287ef4279f74a0d07
anas <***@***.com>
2023-04-11 14:42:30 -0700

    creates an array of chars...

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,7 +7,7 @@
 
 /**
  * create_array - creates an array of chars and
- * 		initializes it with a specific char
+ * initializes it with a specific char
  * @size: size of the array
  * @c: char value in array
  * Return: array otherwise null
