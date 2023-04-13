c4b7213c426a878377dd0195dc709644ee16f113
anas <***@***.com>
2023-04-13 15:06:05 -0700

    allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ * task 0
+ */
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
