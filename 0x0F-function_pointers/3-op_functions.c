4184361ccf763aa15c0a52a67289228572235ec8
anas <***@***.com>
2023-04-19 17:21:07 -0700

    Task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -4,12 +4,6 @@
  * task 3
  */
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
 /**
  * op_add - sum two numbers
  * @a: first number
