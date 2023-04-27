ac3837d59e41b988d505458b6f5323b412ccc361
anas <***@***.com>
2023-04-27 05:10:05 -0700

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,9 +5,9 @@
  */
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: linked list
- * Return: number of nodes
+ * print_list - prints all the elements of a list_t list
+ * @h: list
+ * Return: size
  */
 
 size_t print_list(const list_t *h)
