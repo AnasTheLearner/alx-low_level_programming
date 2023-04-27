f175825cec464412cd6ed4b6faf2566438bfad01
anas <***@***.com>
2023-04-27 03:47:04 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,14 @@
  * @h: linked list
  * Return: number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t a = 0;
 
-	while (h)
+	while(h != NULL)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
