0d1afca185d2054d4d37018fb60cbdc129770eca
anas <***@***.com>
2023-04-27 04:49:52 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -11,6 +11,7 @@ void __attribute__((constructor)) beforemain(void);
  * Return: nothing
  */
 
+void beforemain(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
