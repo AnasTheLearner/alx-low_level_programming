a684b55e7165b77704202e75acaca2e2556e98e9
anas <***@***.com>
2023-04-27 03:31:15 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/*
+ * task 0
+ */
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: linked list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t a = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		a++;
+	}
+
+	return (a);
+}
