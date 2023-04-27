e1ea20f13b8ec06ac0b1a544d935789681f86dac
anas <***@***.com>
2023-04-27 05:15:18 -0700

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,11 +5,11 @@
  */
 
 /**
- * print_list - prints all the elements of a list_t list
+ * print_list - prints all the elements
+ * of a list_t list
  * @h: list
  * Return: size
  */
-
 size_t print_list(const list_t *h)
 {
 	size_t ndes = 0;
@@ -20,8 +20,8 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
 		ndes++;
+		h = h->next;
 	}
 	return (ndes);
 }
