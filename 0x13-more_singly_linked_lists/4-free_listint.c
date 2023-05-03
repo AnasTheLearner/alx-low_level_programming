36fe06dcfd9a755f1d63a8259558e4ca963095c5
anas <***@***.com>
2023-05-03 14:40:23 -0700

    Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/*
+ * task 4
+ */
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: list to free
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *cnt;
+	while (head)
+	{
+		cnt = head->next;
+		free(head);
+		head = cnt;
+	}
+}
