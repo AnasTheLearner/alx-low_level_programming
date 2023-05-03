3093156df7eb14c2fe08a18e99e50ecef1b2a6c2
anas <***@***.com>
2023-05-03 14:05:29 -0700

    print listint_t

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t cnt = 0;
+	size_t ab = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		cnt++;
+		ab++;
 		h = h->next;
 	}
-	return (cnt);
+	return (ab);
 }
