bd23b3252ae0119152ae942dbc216fb35788b8e6
anas <***@***.com>
2023-05-03 14:13:09 -0700

    print listint_t

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,15 @@
 #include "lists.h"
+#include <stdio.h>
+
+/*
+ * task 0
+ */
 
 /**
  * print_listint - prints all the elements
  * of a listint_t list
  * @h: list to print
+ *
  * Return: nodes number
  */
 size_t print_listint(const listint_t *h)
