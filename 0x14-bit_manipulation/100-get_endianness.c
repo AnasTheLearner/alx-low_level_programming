41908bb3d5371a8cc27304107cf54a271b06605e
anas <***@***.com>
2023-05-04 14:42:19 -0700

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * task 6
+ */
+
+/**
+ * get_endianness - checks the endianness.
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int k = 1;
+
+	if (*(char *)&k == 1)
+		return (1);
+	else
+
+		return (0);
+}
