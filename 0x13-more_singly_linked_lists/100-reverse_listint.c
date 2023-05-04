5290ecf7293b59a0a8990c9036f6aea5ee3ad566
anas <***@***.com>
2023-05-03 18:31:43 -0700

    Reverse list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists"
+
+/*
+ * task 11
+ */
+
+/**
+ * reverse_listint - reverses a listint_t linked list
+ * @head: head of the list
+ * Return: a pointer to the first node of the reversed list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *cnt = NULL;
+	listint_t *imt = NULL;
+
+	while (*head)
+	{
+		imt = (*head)->imt;
+		(*head)->imt = cnt;
+		cnt = *head;
+		*head = imt;
+	}
+	*head = cnt;
+	return (*head);
+}
